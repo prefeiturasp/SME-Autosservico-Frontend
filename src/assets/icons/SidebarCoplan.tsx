2acaea66a11cacc46511fb89232a311f47e243a0
ollyvergithub <ollyverottoboni@gmail.com>
2025-07-22 08:57:55 -0300

    feat: :bento: Adiciona ícones ao projeto

---
+++ src/assets/icons/SidebarCoplan.tsx
@@ -0,0 +1,132 @@
+export default function SidebarCoplan(
+    props: Readonly<React.SVGProps<SVGSVGElement>>
+) {
+    return (
+        <svg
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            id="screenshot-b5d546b9-73df-80f5-8006-2c56a8203cd5"
+            viewBox="0 0 24 24"
+            className={props.className}
+            fill="none"
+            version="1.1"
+        >
+            <g
+                id="shape-b5d546b9-73df-80f5-8006-2c56a8203cd5"
+                width="24"
+                height="24"
+                rx="0"
+                ry="0"
+                style={{fill: "rgb(0, 0, 0)"}}
+            >
+                <g
+                    id="shape-b5d546b9-73df-80f5-8006-2c56a8203cd6"
+                    style={{display: "none"}}
+                >
+                    <g
+                        className="fills"
+                        id="fills-b5d546b9-73df-80f5-8006-2c56a8203cd6"
+                    >
+                        <rect
+                            width="24"
+                            height="24"
+                            x="0"
+                            transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+                            style={{fill: "none"}}
+                            ry="0"
+                            fill="none"
+                            rx="0"
+                            y="0"
+                        />
+                    </g>
+                </g>
+                <g
+                    id="shape-b5d546b9-73df-80f5-8006-2c56a8203cd7"
+                    rx="0"
+                    ry="0"
+                    style={{fill: "rgb(0, 0, 0)"}}
+                >
+                    <g id="shape-b5d546b9-73df-80f5-8006-2c56a8203cd8">
+                        <g
+                            className="fills"
+                            id="fills-b5d546b9-73df-80f5-8006-2c56a8203cd8"
+                        >
+                            <path
+                                d="M5.795,14.306L4.023,12.531L2.250,14.306M18.205,9.727L19.977,11.503L21.750,9.727"
+                                fill="none"
+                                strokeLinejoin="round"
+                                style={{fill: "none"}}
+                            />
+                        </g>
+                        <g
+                            fill="none"
+                            strokeLinejoin="round"
+                            id="strokes-b4e81641-7500-808e-8006-8682b2bebc1f-b5d546b9-73df-80f5-8006-2c56a8203cd8"
+                            className="strokes"
+                        >
+                            <g className="stroke-shape">
+                                <path
+                                    d="M5.795,14.306L4.023,12.531L2.250,14.306M18.205,9.727L19.977,11.503L21.750,9.727"
+                                    style={{fill: "none", strokeWidth: 1.5, stroke: "rgb(255, 255, 255)", strokeOpacity: 1}}
+                                />
+                            </g>
+                        </g>
+                    </g>
+                    <g id="shape-b5d546b9-73df-80f5-8006-2c56a8203cd9">
+                        <g
+                            className="fills"
+                            id="fills-b5d546b9-73df-80f5-8006-2c56a8203cd9"
+                        >
+                            <path
+                                d="M19.977,11.503C19.977,9.383,19.137,7.352,17.641,5.853C16.146,4.355,14.116,3.513,12.000,3.513C10.982,3.512,9.974,3.708,9.030,4.090C6.899,4.944,5.239,6.675,4.475,8.840M4.023,12.530C4.025,14.385,4.670,16.181,5.850,17.612C8.281,20.560,12.462,21.369,15.816,19.539C17.442,18.650,18.709,17.224,19.401,15.505"
+                                fill="none"
+                                strokeLinejoin="round"
+                                style={{fill: "none"}}
+                            />
+                        </g>
+                        <g
+                            fill="none"
+                            strokeLinejoin="round"
+                            id="strokes-b4e81641-7500-808e-8006-8682b2bf7a43-b5d546b9-73df-80f5-8006-2c56a8203cd9"
+                            className="strokes"
+                        >
+                            <g className="stroke-shape">
+                                <path
+                                    d="M19.977,11.503C19.977,9.383,19.137,7.352,17.641,5.853C16.146,4.355,14.116,3.513,12.000,3.513C10.982,3.512,9.974,3.708,9.030,4.090C6.899,4.944,5.239,6.675,4.475,8.840M4.023,12.530C4.025,14.385,4.670,16.181,5.850,17.612C8.281,20.560,12.462,21.369,15.816,19.539C17.442,18.650,18.709,17.224,19.401,15.505"
+                                    style={{fill: "none", strokeWidth: 1.5, stroke: "rgb(255, 255, 255)", strokeOpacity: 1}}
+                                />
+                            </g>
+                        </g>
+                    </g>
+                    <g id="shape-b5d546b9-73df-80f5-8006-2c56a8203cda">
+                        <g
+                            className="fills"
+                            id="fills-b5d546b9-73df-80f5-8006-2c56a8203cda"
+                        >
+                            <path
+                                d="M9.580,13.978C9.688,15.213,10.763,16.135,12.000,16.054C13.952,16.054,14.420,14.931,14.420,13.978C14.420,13.025,13.613,12.015,12.000,12.015C10.387,12.015,9.580,11.377,9.580,10.077C9.626,9.155,10.238,8.357,11.117,8.074C11.402,7.982,11.702,7.949,12.000,7.977C13.236,7.923,14.299,8.845,14.420,10.077M12.000,17.264L12.000,16.213M12.000,6.763L12.000,7.973"
+                                fill="none"
+                                strokeLinejoin="round"
+                                style={{fill: "none"}}
+                            />
+                        </g>
+                        <g
+                            fill="none"
+                            strokeLinejoin="round"
+                            id="strokes-b4e81641-7500-808e-8006-8682b2bfa697-b5d546b9-73df-80f5-8006-2c56a8203cda"
+                            className="strokes"
+                        >
+                            <g className="stroke-shape">
+                                <path
+                                    d="M9.580,13.978C9.688,15.213,10.763,16.135,12.000,16.054C13.952,16.054,14.420,14.931,14.420,13.978C14.420,13.025,13.613,12.015,12.000,12.015C10.387,12.015,9.580,11.377,9.580,10.077C9.626,9.155,10.238,8.357,11.117,8.074C11.402,7.982,11.702,7.949,12.000,7.977C13.236,7.923,14.299,8.845,14.420,10.077M12.000,17.264L12.000,16.213M12.000,6.763L12.000,7.973"
+                                    style={{fill: "none", strokeWidth: 1.5, stroke: "rgb(255, 255, 255)", strokeOpacity: 1}}
+                                />
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+}
---
+++ src/assets/icons/SidebarMenuOpen.tsx
@@ -0,0 +1,177 @@
+export default function SidebarMenuOpen(
+    props: Readonly<React.SVGProps<SVGSVGElement>>
+) {
+    return (
+        <svg
+            width="28.767"
+            xmlns="http://www.w3.org/2000/svg"
+            height="28.767"
+            id="screenshot-5bcc641d-caae-80e6-8006-16c1cafc7689"
+            viewBox="-2 -2 28.767 28.767"
+            className={props.className}
+            data-testid="icon-open"
+            fill="none"
+            version="1.1"
+        >
+            <g id="shape-5bcc641d-caae-80e6-8006-16c1cafc7689">
+                <g>
+                    <g>
+                        <g fill="none">
+                            <g
+                                className="fills"
+                                id="fills-5bcc641d-caae-80e6-8006-16c1cafc7689"
+                            >
+                                <rect
+                                    rx="4"
+                                    ry="4"
+                                    x="0"
+                                    y="0"
+                                    width="24.76666666666688"
+                                    height="24.76666666666688"
+                                    transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+                                    className="frame-background"
+                                />
+                            </g>
+                            <g className="frame-children">
+                                <g
+                                    id="shape-5bcc641d-caae-80e6-8006-16c1cafc769e"
+                                    style={{ display: "none" }}
+                                >
+                                    <g
+                                        className="fills"
+                                        id="fills-5bcc641d-caae-80e6-8006-16c1cafc769e"
+                                    >
+                                        <rect
+                                            width="16"
+                                            height="16"
+                                            x="0"
+                                            transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+                                            style={{
+                                                fill: "rgb(255, 255, 255)",
+                                                fillOpacity: 1,
+                                            }}
+                                            ry="0"
+                                            fill="none"
+                                            rx="0"
+                                            y="0"
+                                        />
+                                    </g>
+                                </g>
+                                <g
+                                    id="shape-5bcc641d-caae-80e6-8006-16c20a1277a8"
+                                    width="24"
+                                    height="24"
+                                    rx="0"
+                                    ry="0"
+                                    style={{ fill: "rgb(0, 0, 0)" }}
+                                >
+                                    <g
+                                        id="shape-5bcc641d-caae-80e6-8006-16c20a1361a4"
+                                        style={{ display: "none" }}
+                                    >
+                                        <g
+                                            className="fills"
+                                            id="fills-5bcc641d-caae-80e6-8006-16c20a1361a4"
+                                        >
+                                            <rect
+                                                width="16"
+                                                height="16"
+                                                x="4.383333333333439"
+                                                transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+                                                style={{
+                                                    fill: "rgb(255, 255, 255)",
+                                                    fillOpacity: 1,
+                                                }}
+                                                ry="0"
+                                                fill="none"
+                                                rx="0"
+                                                y="4.383333333333212"
+                                            />
+                                        </g>
+                                    </g>
+                                    <g id="shape-5bcc641d-caae-80e6-8006-16c20a13b542">
+                                        <g
+                                            className="fills"
+                                            id="fills-5bcc641d-caae-80e6-8006-16c20a13b542"
+                                        >
+                                            <path
+                                                d="M7.050,16.383C6.861,16.383,6.703,16.319,6.575,16.191C6.448,16.063,6.384,15.905,6.383,15.717C6.383,15.528,6.447,15.370,6.575,15.242C6.704,15.114,6.862,15.050,7.050,15.050L17.717,15.050C17.906,15.050,18.064,15.114,18.192,15.242C18.320,15.370,18.384,15.528,18.383,15.717C18.383,15.905,18.319,16.064,18.191,16.192C18.064,16.320,17.906,16.384,17.717,16.383ZM7.050,13.050C6.861,13.050,6.703,12.986,6.575,12.858C6.448,12.730,6.384,12.572,6.383,12.383C6.383,12.195,6.447,12.037,6.575,11.909C6.704,11.781,6.862,11.717,7.050,11.717L17.717,11.717C17.906,11.717,18.064,11.781,18.192,11.909C18.320,12.037,18.384,12.195,18.383,12.383C18.383,12.572,18.319,12.730,18.191,12.859C18.064,12.987,17.906,13.051,17.717,13.050ZM7.050,9.717C6.861,9.717,6.703,9.653,6.575,9.525C6.448,9.397,6.384,9.238,6.383,9.050C6.383,8.862,6.447,8.703,6.575,8.575C6.704,8.447,6.862,8.383,7.050,8.383L17.717,8.383C17.906,8.383,18.064,8.447,18.192,8.575C18.320,8.703,18.384,8.862,18.383,9.050C18.383,9.238,18.319,9.397,18.191,9.525C18.064,9.654,17.906,9.718,17.717,9.717Z"
+                                                style={{
+                                                    fill: "rgb(255, 255, 255)",
+                                                    fillOpacity: 1,
+                                                }}
+                                            />
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                    <g
+                        id="strokes-378060a0-ab52-80cc-8006-8689b348c11d-5bcc641d-caae-80e6-8006-16c1cafc7689"
+                        className="strokes"
+                    >
+                        <g className="outer-stroke-shape">
+                            <defs>
+                                <mask
+                                    id="outer-stroke-render-1-5bcc641d-caae-80e6-8006-16c1cafc7689-0"
+                                    x="-1.4142135623730951"
+                                    y="-1.4142135623730951"
+                                    width="27.59509379141307"
+                                    height="27.59509379141307"
+                                    maskUnits="userSpaceOnUse"
+                                >
+                                    <use
+                                        href="#stroke-shape-render-1-5bcc641d-caae-80e6-8006-16c1cafc7689-0"
+                                        style={{
+                                            fill: "none",
+                                            stroke: "white",
+                                            strokeWidth: "2",
+                                        }}
+                                    />
+                                    <use
+                                        href="#stroke-shape-render-1-5bcc641d-caae-80e6-8006-16c1cafc7689-0"
+                                        style={{
+                                            fill: "black",
+                                            stroke: "none",
+                                        }}
+                                    />
+                                </mask>
+                                <rect
+                                    width="24.76666666666688"
+                                    height="24.76666666666688"
+                                    className="frame-background"
+                                    x="0"
+                                    id="stroke-shape-render-1-5bcc641d-caae-80e6-8006-16c1cafc7689-0"
+                                    transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+                                    ry="4"
+                                    rx="4"
+                                    y="0"
+                                />
+                            </defs>
+                            <use
+                                href="#stroke-shape-render-1-5bcc641d-caae-80e6-8006-16c1cafc7689-0"
+                                mask="url(#outer-stroke-render-1-5bcc641d-caae-80e6-8006-16c1cafc7689-0)"
+                                style={{
+                                    fill: "none",
+                                    strokeWidth: "2",
+                                    stroke: "rgb(255, 255, 255)",
+                                    strokeOpacity: "1",
+                                }}
+                            />
+                            <use
+                                href="#stroke-shape-render-1-5bcc641d-caae-80e6-8006-16c1cafc7689-0"
+                                style={{
+                                    fill: "none",
+                                    strokeWidth: "1",
+                                    stroke: "none",
+                                    strokeOpacity: "1",
+                                }}
+                            />
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+}

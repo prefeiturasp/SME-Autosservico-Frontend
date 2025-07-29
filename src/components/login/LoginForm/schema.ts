505913f602cf0412ad8451ef3d8bce7b33dad844
ollyvergithub <ollyverottoboni@gmail.com>
2025-07-29 14:21:30 -0300

    refactor: :recycle: Melhora código implementando constantes

--- src/components/login/LoginForm/schema.ts
+++ src/components/login/LoginForm/schema.ts
@@ -1,13 +1,19 @@
 import { z } from "zod";
 
+import {
+  RF_ERROR_MESSAGE,
+  PASSWORD_ERROR_MESSAGE,
+  RF_FORMAT_ERROR_MESSAGE
+} from "@/const";
+
 const formSchema = z.object({
     rf: z
         .string()
-        .min(1, "RF é obrigatório")
-        .max(8, "Formato de RF inválido"),
+        .min(1, RF_ERROR_MESSAGE)
+        .max(8, RF_FORMAT_ERROR_MESSAGE),
     password: z
         .string()
-        .min(1, "Senha é obrigatória")
+        .min(1, PASSWORD_ERROR_MESSAGE)
 });
 
 export type FormDataLogin = z.infer<typeof formSchema>;

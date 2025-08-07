d353abc7546455b35e00ec6ec569514ca102d2fc
ollyvergithub <ollyverottoboni@gmail.com>
2025-08-07 14:08:33 -0300

    feat: :bento: Aumenta ícones das Squads na Sidebar

--- src/assets/icons/SidebarGipe.tsx
+++ src/assets/icons/SidebarGipe.tsx
@@ -13,77 +13,37 @@ export default function SidebarGipe(
             fill="none"
             version="1.1"
         >
-            <g
-                id="shape-c4a637b9-c250-8011-8006-895c3198b1be"
+            <rect
                 width="24"
                 height="24"
-                rx="0"
+                x="0"
+                transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+                style={{ fill: "none" }}
                 ry="0"
-                style={{ fill: "rgb(0, 0, 0)" }}
-            >
-                <g
-                    id="shape-c4a637b9-c250-8011-8006-895c319c71d3"
-                    style={{ display: "none" }}
-                >
-                    <g
-                        className="fills"
-                        id="fills-c4a637b9-c250-8011-8006-895c319c71d3"
-                    >
-                        <rect
-                            width="24"
-                            height="24"
-                            x="0"
-                            transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
-                            style={{ fill: "none" }}
-                            ry="0"
-                            fill="none"
-                            rx="0"
-                            y="0"
-                        />
-                    </g>
-                </g>
-                <g id="shape-c4a637b9-c250-8011-8006-895c319ccfa4">
-                    <g
-                        className="fills"
-                        id="fills-c4a637b9-c250-8011-8006-895c319ccfa4"
-                    >
-                        <path
-                            d="M12.000,2.000L3.000,5.500L3.000,12.000C3.000,15.500,6.500,21.000,11.000,22.000C15.500,21.000,19.000,15.500,19.000,12.000L19.000,5.500L11.000,2.000Z"
-                            fill="none"
-                            strokeLinejoin="round"
-                            style={{ fill: "none" }}
-                        />
-                    </g>
-                    <g
-                        fill="none"
-                        strokeLinejoin="round"
-                        id="strokes-c88facb2-4ec2-8078-8006-94f5e50f7c90-c4a637b9-c250-8011-8006-895c319ccfa4"
-                        className="strokes"
-                    >
-                        <g className="inner-stroke-shape">
-                            <defs>
-                                <clipPath id="inner-stroke-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0">
-                                    <use href="#stroke-shape-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0" />
-                                </clipPath>
-                                <path
-                                    d="M12.000,2.000L3.000,5.500L3.000,12.000C3.000,15.500,6.500,21.000,11.000,22.000C15.500,21.000,19.000,15.500,19.000,12.000L19.000,5.500L11.000,2.000Z"
-                                    id="stroke-shape-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0"
-                                    style={{
-                                        fill: "none",
-                                        strokeWidth: 4,
-                                        stroke: "rgb(255, 255, 255)",
-                                        strokeOpacity: 1,
-                                    }}
-                                />
-                            </defs>
-                            <use
-                                href="#stroke-shape-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0"
-                                clipPath="url('#inner-stroke-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0')"
-                            />
-                        </g>
-                    </g>
-                </g>
-            </g>
+                fill="none"
+                rx="0"
+                y="0"
+            />
+
+            <defs>
+                <clipPath id="inner-stroke-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0">
+                    <use href="#stroke-shape-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0" />
+                </clipPath>
+                <path
+                    d="M12.000,2.000L3.000,5.500L3.000,12.000C3.000,15.500,6.500,21.000,11.000,22.000C15.500,21.000,19.000,15.500,19.000,12.000L19.000,5.500L11.000,2.000Z"
+                    id="stroke-shape-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0"
+                    style={{
+                        fill: "none",
+                        strokeWidth: 4,
+                        stroke: "rgb(255, 255, 255)",
+                        strokeOpacity: 1,
+                    }}
+                />
+            </defs>
+            <use
+                href="#stroke-shape-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0"
+                clipPath="url('#inner-stroke-render-3-c4a637b9-c250-8011-8006-895c319ccfa4-0')"
+            />
         </svg>
     );
 }

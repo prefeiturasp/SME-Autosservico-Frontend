b61504c8dce0ee3fc3cadf0ab8d357a8af2404a7
ollyvergithub <ollyverottoboni@gmail.com>
2025-09-10 11:46:14 -0300

    feat: :sparkles: Implementa componente Saúde Dos Servidores Filas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,42 @@
 "use client";
-import  CardWrapperInfoAmbientes from "@/components/dashboard/CardWrapperInfoAmbientes";
+import CardWrapperInfoAmbientes from "@/components/dashboard/CardWrapperInfoAmbientes";
 import Producao from "@/components/dashboard/DisponibilidadeDosAmbientes/Producao";
+import Filas from "@/components/dashboard/SaudeDosServidores/Filas";
 import useDashboardStore from "@/states/dashboard";
 
 export default function Dashboard() {
-
     const activeProject = useDashboardStore((state) => state.activeProject);
     const projectNameFrontEnd = activeProject?.zabbixQueryFrontend?.trim();
     const projectNameBackEnd = activeProject?.zabbixQueryBackend?.trim();
+    const projectNameFilasRabbitMQ = activeProject?.zabbixQueryFilasRabbitMQ?.trim();
 
     return (
         <div className="border-b bg-background px-6 py-4">
             <div className="flex space-x-4 mb-6">
-                <CardWrapperInfoAmbientes title = "Disponibilidade do ambiente" className="max-w-sm mb-5">
-                    <Producao projectName={projectNameFrontEnd ?? ""} />
+                <CardWrapperInfoAmbientes
+                    title="Disponibilidade do ambiente"
+                    className="max-w-sm mb-5"
+                >
+                    <Producao
+                        className="bg-[#F5F5F5] p-3"
+                        projectName={projectNameFrontEnd ?? ""}
+                    />
                 </CardWrapperInfoAmbientes>
 
-                <CardWrapperInfoAmbientes title = "Saúde do servidor (Workloads)" className="max-w-sm mb-5">
-                    <Producao title="API Service" projectName={projectNameBackEnd ?? ""} />
+                <CardWrapperInfoAmbientes
+                    title="Saúde do servidor (Workloads)"
+                    className="max-w-sm mb-5"
+                >
+                    <Filas
+                        title="Fila"
+                        className="mb-5 bg-[#F5F5F5] p-3"
+                        projectName={projectNameFilasRabbitMQ ?? ""}
+                    />
+                    <Producao
+                        title="API Service"
+                        className="bg-[#F5F5F5] p-3"
+                        projectName={projectNameBackEnd ?? ""}
+                    />
                 </CardWrapperInfoAmbientes>
             </div>
         </div>

7fe8999ee7e0e8308dedca4939b81aab371d2543
Luís Dourado <luis_dourado33@hotmail.com>
2025-12-17 15:38:36 -0400

    feat: Expandir o esquema Sistema para incluir informações de subprojetos Jenkins e atualizar testes correspondentes

--- src/components/dashboard/SelectSistemas/schema.test.ts
+++ src/components/dashboard/SelectSistemas/schema.test.ts
@@ -3,7 +3,15 @@ import { SistemaSchema, SelectedSistemaSchema } from "./schema";
 
 describe("SistemaSchema", () => {
     it("deve validar um objeto Sistema válido", () => {
-        const validSistema = { id: "10", nome: "Novo SGP", zabbixQueryFrontend: "PRD - Novo SGP", zabbixQueryBackend: "PRD - Novo SGP - API", zabbixQueryFilasRabbitMQ: "PRD - RabbitMQ" };
+        const validSistema = {
+            id: "10",
+            nome: "Novo SGP",
+            zabbixQueryFrontend: "PRD - Novo SGP",
+            zabbixQueryBackend: "PRD - Novo SGP - API",
+            zabbixQueryFilasRabbitMQ: "PRD - RabbitMQ",
+            zabbixQueryJenkinsJob: "SME-NovoSGP-Docs/master",
+            jenkinsSubprojects: [{ label: "SME-NovoSGP", key: "SME-NovoSGP" }],
+        };
         const result = SistemaSchema.safeParse(validSistema);
 
         expect(result.success).toBe(true);

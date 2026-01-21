d0c48bdd6c37068a403c7caec7a1136b8bdb98fe
Wilson Santos <wilson.santos@spassu.com.br>
2026-01-21 11:31:29 -0300

    Ajuste automacao

--- testes/ui/cypress/support/e2e.js
+++ testes/ui/cypress/support/e2e.js
@@ -1,6 +1,7 @@
 import '@shelex/cypress-allure-plugin'
 import "cypress-cloud/support";
 import './commands_ui/commands_login'
+import './commands_ui/commands_sidebar'
 import 'cypress-xpath'
 
 // PostgreSQL
--- testes/ui/cypress/support/step_definitions/monitoramento.js
+++ testes/ui/cypress/support/step_definitions/monitoramento.js
@@ -2,48 +2,98 @@ import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 import Login_Auto_Servico_Localizadores from '../locators/login_locators'
 import Monitoramento_Auto_Servico_Localizadores from '../locators/monitoramento_locators'
 
-const locators = new Login_Auto_Servico_Localizadores()
-const locators_monitoramento = new Monitoramento_Auto_Servico_Localizadores()
+const loginLocators = new Login_Auto_Servico_Localizadores()
+const monitoramentoLocators = new Monitoramento_Auto_Servico_Localizadores()
 
 
-Given(/^que eu estou na página de login$/, () => {
+Given('que eu acesso o sistema', () => {
   cy.login_autoservico()
 })
 
-When(/^eu insiro o RF "([^"]*)" e senha "([^"]*)"$/, (rf, senha) => {
-  if (rf) {
-    cy.get(locators.campo_usuario()).clear().type(rf)
-  } else {
-    cy.get(locators.campo_usuario()).clear()
-  }
 
-  if (senha) {
-    cy.get(locators.campo_senha()).clear().type(senha)
-  } else {
-    cy.get(locators.campo_senha()).clear()
+When(
+  'eu informo o RF do tipo {string} e a senha do tipo {string}',
+  (tipo_rf, tipo_senha) => {
+
+    const massaLogin = {
+      rf_valido: Cypress.env('RF_VALIDO'),
+      senha_valida: Cypress.env('SENHA_VALIDA'),
+
+      rf_invalido: Cypress.env('RF_INVALIDO'),
+      senha_invalida: Cypress.env('SENHA_INVALIDA')
+    }
+
+    const rf = massaLogin[tipo_rf]
+    const senha = massaLogin[tipo_senha]
+
+    // proteção extra (MESMO PADRÃO DO SIDEBAR)
+    if (rf === undefined || senha === undefined) {
+      throw new Error(`Massa não encontrada: rf=${tipo_rf}, senha=${tipo_senha}`)
+    }
+
+    cy.get(loginLocators.campo_usuario())
+      .should('be.visible')
+      .clear()
+      .type(rf)
+
+    cy.get(loginLocators.campo_senha())
+      .should('be.visible')
+      .clear()
+      .type(senha)
   }
+)
+
+When('clico no botão de acessar', () => {
+  cy.botao_acessar().click()
 })
 
-When(/^clico no botão de Entrar$/, () => {
-  cy.get('button')
-    .filter((_, el) => el.innerText.trim() === 'Entrar')
-    .click()
-})
+Then(
+  'o resultado esperado para o cenário {string} deve ser exibido',
+  (cenario) => {
 
-Then(/^devo ter acesso ao dashboard$/, () => {
+    switch (cenario) {
+      case 'Login válido padrão':
+        cy.url().should('not.include', 'login')
+        break
+
+      case 'Login inválido':
+        cy.contains('Vamos tentar de novo?').should('be.visible')
+        break
+
+      default:
+        throw new Error(`Cenário não mapeado: ${cenario}`)
+    }
+  }
+)
+
+Then('devo ter acesso ao dashboard', () => {
   cy.url().should('include', '/dashboard')
 })
 
-When(/^clico no menu Acom$/, () => {
-  cy.get(locators_monitoramento.botao_ascom()).click()
+
+When('clico no menu {string}', (menu) => {
+  const mapaMenu = {
+    ASCOM: monitoramentoLocators.botao_ascom()
+  }
+
+  const locator = mapaMenu[menu]
+
+  if (!locator) {
+    throw new Error(`Menu não mapeado: ${menu}`)
+  }
+
+  cy.get(locator)
+    .should('be.visible')
+    .click()
 })
 
-When(/^seleciono o sistema Portal Educação$/, () => {
-  cy.xpath(locators_monitoramento.botao_sistema()).click()
-  .click({ force: true });
+When('seleciono o sistema {string}', (sistema) => {
+  cy.xpath(monitoramentoLocators.botao_sistema())
+    .should('be.visible')
+    .click({ force: true })
 })
 
-Then(/^devo ver se há algum incidente no sistema$/, () => {
-  cy.xpath(locators_monitoramento.status_sistema())
+Then('devo ver se há algum incidente no sistema', () => {
+  cy.xpath(monitoramentoLocators.status_sistema())
+    .should('exist')
 })
-

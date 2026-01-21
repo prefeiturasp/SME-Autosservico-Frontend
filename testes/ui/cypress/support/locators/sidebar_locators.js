13d22e4e5e9528b7aa61660972da9eb710e353c6
Wilson Santos <wilson.santos@spassu.com.br>
2026-01-21 09:42:10 -0300

    Adicionando novos cenários

---
+++ testes/ui/cypress/support/locators/sidebar_locators.js
@@ -0,0 +1,14 @@
+export const SIDEBAR = {
+  ROOT: '[data-testid="sidebar-root"]',
+  LOGO: 'img[alt="Logo AutoServiço"]',
+  CLOSE_BUTTON: '[data-testid="icon-close"]',
+
+  MENU: {
+    ASCOM: '[data-testid="sidebar-button-ascom"]',
+    CODAE: '[data-testid="sidebar-button-codae"]',
+    COPED: '[data-testid="sidebar-button-coped"]',
+    COPLAN: '[data-testid="sidebar-button-coplan"]',
+    COTIC: '[data-testid="sidebar-button-cotic"]',
+    COGEP: '[data-testid="sidebar-button-cogep"]',
+  }
+}
